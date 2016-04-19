4be7fc075fd2ae6b62dcb16f212de7fe87f9ae44
Egor Golysehv <g.golyshev@forecast.ru>
2016-04-19 18:39:11 +0300

    Питон3 с Виндов

--- week5/main.py
+++ week5/main.py
@@ -1,14 +1,14 @@
-import urllib
+import urllib.request
 import xml.etree.ElementTree as ET
 
 if __name__ == '__main__':
     #serviceurl = 'http://python-data.dr-chuck.net/comments_42.xml'
     serviceurl = 'http://python-data.dr-chuck.net/comments_221445.xml'
-    url=raw_input('Enter location :')
+    url=input('Enter location :')
     if len(url)<1:
         url=serviceurl
     print('Retrieving: ' + url)
-    uh=urllib.urlopen(url)
+    uh=urllib.request.urlopen(url )
     data=uh.read()
     print('Retrived ' + str(len(data)) + ' characters')
     tr=ET.fromstring(data)
